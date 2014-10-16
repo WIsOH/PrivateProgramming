335c6953003f119040a0c49df4071d56a21729e4
koding user <isaacohern97@gmail.com>
2014-10-16 17:36:38 +0000

    new file:   FLVS_AP_COMP_SCI_A/TextMessageV1.class 	new file:   FLVS_AP_COMP_SCI_A/TextMessageV1.java

---
+++ FLVS_AP_COMP_SCI_A/TextMessageV1.java
@@ -0,0 +1,12 @@
+/**
+*@author Isaac O'Hern
+*@version 10/16/2014
+**/
+
+public class TextMessageV1 
+{
+    public static void main(String arg[])
+    {
+	String textMessage = "In that case, could we do the call later tonight once I get home?";
+    }
+}
