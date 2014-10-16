56ef052cf473c63bcbc40eaf638d25813f5f76cf
Isaac O'Hern <isaacohern97@gmail.com>
2014-10-16 07:49:54 -0400

    Restructure and added first project

---
+++ FLVS_AP_COMP_SCI_A/StudentInfoCard.java
@@ -0,0 +1,33 @@
+/**
+* Show formatted student info card
+*
+*/
+public class StudentInfoCard
+{
+	public static void main(String[] args)
+	{
+	    System.out.println("***********************************************************************");
+	    System.out.println("*                                                                     *");
+	    System.out.println("*                         Student Info Card                           *");
+	    System.out.println("*                                                                     *");
+	    System.out.println("***********************************************************************");
+	
+	    System.out.print("\nName: Isaac O'Hern\t");
+	    System.out.print("Birthday: 08/15/1997\t");
+	    System.out.println("Age: 17");
+
+	    System.out.print("School: Gainesville High School\t");
+	    System.out.print("Grade: 11\t");
+	    System.out.println("City: Gainesville");
+
+	    System.out.print("Cell Phone: 352-363-7947\t");
+	    System.out.println("Availabe 6-9 M-Th and 3-9 F-Su");
+
+	    System.out.println("Current Math: None");
+	    System.out.println("Previous Programming Experiance: Messing with C, Java, and Python");
+
+	    System.out.println("\nI am taking this course to learn more of the technical part of \n" +
+			       "computer science, and am intereseted in taking it in college so \n" +
+			       "getting a credit would be nice.");
+	}
+}
