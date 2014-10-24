208656ce0e5dcdb27a5c46160754eec17c083771
WIsOH <WilliamIOHern@gmail.com>
2014-10-24 15:46:59 -0400

    Did Work

--- FLVS_AP_COMP_SCI_A/WellnessMenu.java
+++ FLVS_AP_COMP_SCI_A/WellnessMenu.java
@@ -1,8 +1,8 @@
-/**
+/*
  * Prompts user to select a category and tests the menu structure.
  * 
- * @author  
- * @version 
+ * @author William Isaac O"Hern 
+ * @version 10/24/2014
  */
 import java.util.Scanner;
 public class WellnessMenu
@@ -10,42 +10,50 @@ public class WellnessMenu
     public static void main(String [] args)
     {
         Scanner in = new Scanner(System.in);
+
+	String selection;
         
         System.out.println("Please select one of the following options from the menu for more information.");
         System.out.println("\nSelect a letter corresponding to a menu option.");
         System.out.println("[A] BMI");
-        // provide menu item for BMR
-        // provide menu item for Healthy Eating
-        // provide menu item for Food Pyramid
-        // provide menu item for Fitness Training
-        // prompt user to enter A, B, C, D, or E: ");
-        // accept user choice with a Scanner class method
+        System.out.println("[B] BMR");
+        System.out.println("[C] Healthy Eating");
+        System.out.println("[D] Food Pyramid");
+        System.out.println("[E] Fitness Training");
         System.out.println();
         
-        if( ) //condition for choice A goes in the parentheses
-        {
+	selection = in.next().toUpperCase();
+	System.out.println(selection);
+
+        if(selection.equals("A"))
+	{
             // provide print statement to indicate menu item A was chosen
             System.out.println("Testing: You chose A for BMI");
         }
-        else if( ) //condition for choice B goes in the parentheses
+        else if(selection.equals("B")) //condition for choice B goes in the parentheses
         {
             // provide print statement to indicate menu item B was chosen
+            System.out.println("Testing: You chose B for BMR");
         }
-        else if ( ) //condition for choice C goes in the parentheses
+        else if (selection.equals("C")) //condition for choice C goes in the parentheses
         {
             // provide print statement to indicate menu item C was chosen
+            System.out.println("Testing: You chose C for Healthy Eating");
         }
-        else if ( ) //condition for choice D goes in the parentheses
+        else if (selection.equals("D")) //condition for choice D goes in the parentheses
         {
             // provide print statement to indicate menu item D was chosen
+            System.out.println("Testing: You chose D for Food Pyramid");
         }
-        else if ( ) //condition for choice E goes in the parentheses
+        else if (selection.equals("E")) //condition for choice E goes in the parentheses
         {
             // provide print statement to indicate menu item E was chosen
+            System.out.println("Testing: You chose E for Fitness Training");
         }
         else //default choice for an invalid entry
         {
             // provide print statement to indicate invalid entry
+            System.out.println("Testing: You chose an incorrect option");
         }
     }
 }
